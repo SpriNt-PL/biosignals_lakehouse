95e082554ba33fb2069b734a2ef7288cd01d7aaf
Kuba <kuba@example.com>
2026-06-05 17:42:50 +0200

    Proper filter accumulating all emotions

--- models/silver_clean/silver_clean_emotions.sql
+++ models/silver_clean/silver_clean_emotions.sql
@@ -19,8 +19,8 @@ SELECT DISTINCT
     CAST(emotions.SAD AS FLOAT) AS emotion_sad,
     CAST(emotions.ANGRY AS FLOAT) AS emotion_angry,
     CAST(emotions.SURPRISE AS FLOAT) AS emotion_suprise,
-    CAST(emotions.CONTEMPT AS FLOAT) AS emotion_contempt,
-    CAST(emotions.FOCUS AS FLOAT) AS emotion_focus,
+    CAST(emotions.DISGUST AS FLOAT) AS emotion_disgust,
+    CAST(emotions.NEUTRAL AS FLOAT) AS emotion_neutral,
     CAST(dominant AS VARCHAR) AS dominant,
     CAST(confidence AS FLOAT) AS confidence
 FROM raw_data
@@ -31,6 +31,8 @@ WHERE ts IS NOT NULL
   AND emotions.SAD BETWEEN 0.0 AND 1.0
   AND emotions.ANGRY BETWEEN 0.0 AND 1.0
   AND emotions.SURPRISE BETWEEN 0.0 AND 1.0
-  AND emotions.CONTEMPT BETWEEN 0.0 AND 1.0
-  AND emotions.FOCUS BETWEEN 0.0 AND 1.0
-  AND confidence BETWEEN 0.0 AND 1.0
+  AND emotions.DISGUST BETWEEN 0.0 AND 1.0
+  AND emotions.NEUTRAL BETWEEN 0.0 AND 1.0
+--   AND (emotions.HAPPY + emotions.SAD + emotions.ANGRY + emotions.SURPRISE + emotions.DISGUST + emotions.NEUTRAL) = 1
+  AND (emotions.HAPPY + emotions.SAD + emotions.ANGRY + emotions.SURPRISE + emotions.DISGUST + emotions.NEUTRAL) BETWEEN 0.99 AND 1.01
+  AND confidence BETWEEN 0.0 AND 1.0 
