99298b159ae5e3b21f731f85f978379646566562
Anna <s198305@student.pg.edu.pl>
2026-06-06 18:46:23 +0200

    integrated first

---
+++ models/silver_integrated/silver_integrated_biosignals.sql
@@ -0,0 +1,41 @@
+{{ config(
+    materialized='external',  
+    location='s3://silver-integrated/biosignals/data.parquet'
+)
+}}
+
+WITH source_data AS (
+    SELECT * FROM read_parquet('s3://silver-clean/biosignals/data.parquet')
+),
+
+session_start AS (
+    SELECT
+        participation_id,
+        MIN(timestamp) AS first_timestamp
+    FROM source_data
+    GROUP BY participation_id
+),
+
+integrated_buckets AS (
+    SELECT
+        d.participation_id,
+        d.timestamp,
+        d.gsr,
+        d.ppg,
+        d.hr,
+        epoch_ms(d.timestamp)-epoch_ms(s.timestamp) AS ms_from_start,
+        floor((epoch_ms(d.timestamp)-epoch_ms(s.timestamp))/300)*300 AS bucket_ms,
+        s.first_timestamp+INTERVAL (bucket_ms) MILLISECOND AS integrated_timestamp
+    FROM source_data d
+    JOIN session_start s ON d.participation_id=s.participation_id
+)
+
+SELECT
+    participation_id,
+    integrated_timestamp AS timestamp,
+    ROUND(AVG(gsr),3) AS gsr,
+    ROUND(AVG(ppg),3) AS ppg,
+    ROUND(AVG(hr),3) AS hr,
+FROM integrated_buckets
+GROUP BY participation_id, integrated_timestamp
+ORDER BY participation_id, integrated_timestamp
