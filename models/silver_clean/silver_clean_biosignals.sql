b2f4be8e0fe7247417a8209e2e3edfaf33d1a8af
Kuba <kuba@example.com>
2026-06-15 17:08:52 +0200

    Updated the id column name

--- models/silver_clean/silver_clean_biosignals.sql
+++ models/silver_clean/silver_clean_biosignals.sql
@@ -9,11 +9,8 @@ WITH raw_data AS (
     SELECT * FROM read_json_auto('s3://biosignals-bucket/topics/biosignal-data/partition=0/*.json')
 )
 
--- TEMPORARY as we do not have data concerining sessions and participants
--- so we can not create PARTICIPATION for now and in the result we do not have
--- participation_id. 
 SELECT DISTINCT
-    CAST(student_id AS VARCHAR) AS participation_id, -- TO BE CHANGED
+    CAST(participation_id AS VARCHAR) AS participation_id, -- ALREADY CHANGED, PROPER ID
     epoch_ms(CAST(ts AS BIGINT)) AS timestamp,
     CAST(gsr AS FLOAT) AS gsr,
     CAST(ppg AS FLOAT) AS ppg,
@@ -21,7 +18,7 @@ SELECT DISTINCT
 FROM raw_data
 
 WHERE ts IS NOT NULL
-  AND student_id IS NOT NULL
+  AND participation_id IS NOT NULL
   AND gsr IS NOT NULL AND gsr >= 0
   AND ppg IS NOT NULL AND ppg >= 0
   AND hr IS NOT NULL AND hr BETWEEN 0 AND 250
